2a3e5c1908d883a3fc310110454536607c543f3f
Alastair Robertson <ajor@meta.com>
2025-01-10 07:24:04 -0800

    Link to latest docs version

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <Link to="https://docs.bpftrace.org/">
+          <Link to="https://docs.bpftrace.org/0.22" target="_self">
           <img
             className={styles.heroImage}
             src={useBaseUrl('img/bpftrace_Full_Logo-Black.svg')}
